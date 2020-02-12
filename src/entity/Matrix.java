aea84c5d3db4185cfc960dae9c6411b85e8c7478
Jane Sonina <janeridebmx@gmail.com>
2020-02-12 16:48:01 +0300

    Add entity

---
+++ src/entity/Matrix.java
@@ -0,0 +1,19 @@
+package entity;
+
+public class Matrix {
+    private int[][] matrix;
+
+    public Matrix(int size) {
+        matrix = new int[size][size];
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+
+}

626e4348d0f2eba7a63966882bc72e09b50a72c5
Jane Sonina <janeridebmx@gmail.com>
2020-02-14 19:07:35 +0300

    Safe commit 2

---
+++ src/$redundant/Test.java
@@ -0,0 +1,9 @@
+package $redundant;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+}
--- src/$redundant/Try.java
+++ src/$redundant/Try.java
@@ -5,20 +5,46 @@ import entity.Matrix;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Try {
+public class Try extends Thread {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch cdl = new CountDownLatch(5);
+
+        ExecutorService es = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 5; i++) {
+            es.submit(new Try(5, cdl));
+        }
+        es.shutdown();
+        cdl.await();
+    }
+
+    CountDownLatch countDownLatch;
     static Random random = new Random();
-
     static ArrayList<Integer> list;
     static Matrix matrix;
-    int sum;
+    //int sum;
+
+    Try(int n, CountDownLatch cdl) {
+        matrix = new Matrix(n);
+        list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        countDownLatch = cdl;
+    }
 
     public void run() {
-        int n = -1;
-        while (n == -1) {//пробуем пока нет свободных "разрешений"
-            n = getPermission();
-        }
+//        int n = -1;
+//        while (n == -1) {//пробуем пока нет свободных "разрешений"
+//            n = getPermission();
+//        }
         //как только получили - продолжаем
+        int n = getPermission();
 
         this.fillMatrix(n);//нужен ли this?
         //матриц заполняется
@@ -35,11 +61,11 @@ public class Try {
         return -1;
     }
 
-    public void returnPermission(int number){
+    public void returnPermission(int number) {
         list.add(number);
     }
 
-    public void fillMatrix(int n){
+    public void fillMatrix(int n) {
         matrix.getMatrix()[n][n] = Integer.parseInt(Thread.currentThread().getName());
         //чекай,чтобы имена были парсящиеся. где-нибудь
         //заполняет диагональный элемент
@@ -52,32 +78,30 @@ public class Try {
 
 
     /*
-    * метод заполняет рандомный элемент
-    * который не диагональный
-    * */
-    public void fillRandomNElement(int n){
+     * метод заполняет рандомный элемент
+     * который не диагональный
+     * */
+    public void fillRandomNElement(int n) {
         int randomNumber = getRandomPlace(n); //получает рандомную !=n ячейку
         boolean isColumn = random.nextBoolean(); //решает строка или столбец
 
-        if (isColumn){
+        if (isColumn) {
             matrix.getMatrix()[n][randomNumber] = n;
-        }else{
+        } else {
             //рандомное заполнение будет в строке
             matrix.getMatrix()[randomNumber][n] = n;
         }
     }
 
     /*
-    * получает рандомный элемент не равный n  [0...matrix.length-1]
-    * */
-    private int getRandomPlace(int n){
+     * получает рандомный элемент не равный n  [0...matrix.length-1]
+     * */
+    private int getRandomPlace(int n) {
         int randomNumber = n;
 
-        while(randomNumber == n){
-            randomNumber =  (int) (Math.random() * matrix.getMatrix().length-1);
+        while (randomNumber == n) {
+            randomNumber = (int) (Math.random() * matrix.getMatrix().length - 1);
         }
         return n;
     }
-
-
 }
